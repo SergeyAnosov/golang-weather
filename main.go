fee2e524bd81ce74a318a21ddb182fd9bd4ec682
SergeyAnosov <anosov-sa@yandex.ru>
2024-10-17 14:39:57 +0300

    flags

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Новый проект")
+	name := flag.String("name", "Sergey", "Имя пользователя")
+	age := flag.Int("age", 36, "Возраст пользователя")
+	isAdmin := flag.Bool("isAdmin", true, "Администратора")
+
+	flag.Parse()
+	fmt.Println("Имя пользователя: ", *name)
+	fmt.Println("Возраст пользователя: ", *age)
+	fmt.Println("Является администратором: ", *isAdmin)
 }
