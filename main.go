c60ad690c550c029e2eddc560fe69e5456c1f21f
SergeyAnosov <anosov-sa@yandex.ru>
2024-10-18 09:15:21 +0300

    first http

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"golang/weather/geo"
 )
 
 func main() {
 	fmt.Println("Новый проект")
-	name := flag.String("name", "Sergey", "Имя пользователя")
-	age := flag.Int("age", 36, "Возраст пользователя")
-	isAdmin := flag.Bool("isAdmin", true, "Администратора")
+	city := flag.String("city", "", "Город пользователя")
+	// format := flag.Int("format", 1, "Формат вывода погоды")
 
 	flag.Parse()
-	fmt.Println("Имя пользователя: ", *name)
-	fmt.Println("Возраст пользователя: ", *age)
-	fmt.Println("Является администратором: ", *isAdmin)
+
+	fmt.Println("city: ", *city)
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(geoData)
 }
