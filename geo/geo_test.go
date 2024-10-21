4649a70a96a2a3574f0e5df644a461a8f8718553
SergeyAnosov <anosov-sa@yandex.ru>
2024-10-21 13:52:07 +0300

    geo_test

---
+++ geo/geo_test.go
@@ -0,0 +1,11 @@
+package geo_test
+
+import "testing"
+
+func TestGetMyLocation_(t *testing.T) {
+	// Arange - подготовка, expected result, данные для функции
+
+	// Act - вызываем функцию
+
+	// Assert - проверка результата с ожиданием
+}
