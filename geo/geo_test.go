0f3e56a717395b33780a9d33750da547ba08e825
SergeyAnosov <anosov-sa@yandex.ru>
2024-10-22 17:55:24 +0300

    negative test

--- geo/geo_test.go
+++ geo/geo_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestGetMyLocation_(t *testing.T) {
-	// Arange - подготовка, expected result, данные для функции
+func TestGetMyLocation(t *testing.T) {
+	// Arrange - подготовка, expected result, данные для функции
 	city := "London"
 	expected := geo.GeoData{
 		City: "London",
@@ -17,9 +17,17 @@ func TestGetMyLocation_(t *testing.T) {
 	
 	// Assert - проверка результата с ожиданием
 	if err != nil {
-		t.Error(err.Error())
+ 		t.Error(err.Error())
 	}
 	if got.City != expected.City {
 		t.Errorf("Ожидалось %v, получено %v", expected, got)
 	}	
+}
+
+func TestGetMyLocation_No_City(t *testing.T) {
+	city := "LondonGDMNDKV"
+	_, err := geo.GetMyLocation(city)
+	if err != geo.ErrorNoCity {
+		t.Errorf("Ожидалось %v, получено %v", geo.ErrorNoCity, err)
+	}
 }
